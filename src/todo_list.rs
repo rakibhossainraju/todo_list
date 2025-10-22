ab6c4e51e555a44790c123910e78ba7ae5697ca0
rakibhossainraju <rakibhossianraju@gmail.com>
2025-10-23 01:04:33 +0600

    Refactor TodoList component to pass add_todo function to InputSection for adding new todos

--- src/todo_list.rs
+++ src/todo_list.rs
@@ -1,13 +1,23 @@
-use crate::components::InputSection;
-
+use crate::components::*;
 use dioxus::prelude::*;
+
+pub type TodosType = Vec<Todo>;
 
 #[component]
 pub fn TodoList() -> Element {
+    let mut todos: Signal<TodosType> = use_signal(Vec::new);
+
+    let add_todo = move |todo: Todo| {
+        let mut current_todos = todos.read().to_vec();
+        current_todos.push(todo);
+        todos.set(current_todos);
+    };
+
     rsx! {
         div {
             h1 { "Todo List" }
-            InputSection {}
+            InputSection { add_todo }
+            TodosSection {}
         }
     }
 }
