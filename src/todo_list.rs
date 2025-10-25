28471e4f3564bc273fe58104f92c715dfc215377
rakibhossainraju <rakibhossianraju@gmail.com>
2025-10-25 21:09:23 +0600

    Refactor TodoList component to use Signal for todos management and simplify state updates

--- src/todo_list.rs
+++ src/todo_list.rs
@@ -1,23 +1,21 @@
 use crate::components::*;
 use dioxus::prelude::*;
 
-pub type TodosType = Vec<Todo>;
+pub type TodosType = Signal<Vec<Todo>>;
 
 #[component]
 pub fn TodoList() -> Element {
-    let mut todos: Signal<TodosType> = use_signal(Vec::new);
+    let mut todos: TodosType = use_signal(Vec::new);
 
     let add_todo = move |todo: Todo| {
-        let mut current_todos = todos.read().to_vec();
-        current_todos.push(todo);
-        todos.set(current_todos);
+        todos.write().push(todo);
     };
 
     rsx! {
         div {
             h1 { "Todo List" }
             InputSection { add_todo }
-            TodosSection { todos: todos.read().clone() }
+            TodosSection { todos }
         }
     }
 }
